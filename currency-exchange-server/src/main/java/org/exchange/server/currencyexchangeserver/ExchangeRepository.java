de20cca75dbf6d0365dd73a57af4c8a606be412e
unknown <ashwanisingh835@gmail.com>
2019-06-19 21:54:11 +0530

    start afresh

--- currency-exchange-server/src/main/java/org/exchange/server/currencyexchangeserver/ExchangeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.exchange.server.currencyexchangeserver;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeRepository extends JpaRepository<Exchange, Long>{
- Exchange findByFromAndTo(String from,String to);
-}
